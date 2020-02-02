31d57d851289fb1e45de4f04d0bfa6f96e51ac4d
TinManTex <tinman.squad@gmail.com>
2020-02-03 07:09:04 +1300

    lapi: include some headers that were commented out

--- IHHook/lua/lapi.cpp
+++ IHHook/lua/lapi.cpp
@@ -15,18 +15,18 @@
 
 #include "lua.h"
 
-//#include "lapi.h"
-//#include "ldebug.h"
-//#include "ldo.h"
-//#include "lfunc.h"
-//#include "lgc.h"
-//#include "lmem.h"
+#include "lapi.h"
+#include "ldebug.h"
+#include "ldo.h"
+#include "lfunc.h"
+#include "lgc.h"
+#include "lmem.h"
 #include "lobject.h"
 #include "lstate.h"
-//#include "lstring.h"
-//#include "ltable.h"
-//#include "ltm.h"
-//#include "lundump.h"
+#include "lstring.h"
+#include "ltable.h"
+#include "ltm.h"
+#include "lundump.h"
 #include "lvm.h"
 
 
