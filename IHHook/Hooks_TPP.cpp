78cbe7e3fd32ddf96bd995373df39680317b944d
TinManTex <tinman.squad@gmail.com>
2021-01-18 09:47:35 +1300

    UnkSomePlayerUpdateFuncHook

--- IHHook/Hooks_TPP.cpp
+++ IHHook/Hooks_TPP.cpp
@@ -2,18 +2,30 @@
 #include "IHHook.h"//BaseAddr
 #include "spdlog/spdlog.h"
 
-namespace IHHook {
-	extern void* RebasePointer(const size_t address);
+#include "MinHook/MinHook.h"
+#include "HookMacros.h"
 
+namespace IHHook {
 	namespace Hooks_TPP {
 		uintptr_t missionCode_Addr = 0x142A58A00;
 		//uint32_t* missionCode;//tex in header
+
+		HOOKFUNC(void, UnkSomePlayerUpdateFunc, uintptr_t unkPlayerClass, uintptr_t playerIndex)
+		HOOKPTR(UnkSomePlayerUpdateFunc, 0x146e3a620)
+		
+		void UnkSomePlayerUpdateFuncHook(intptr_t unkPlayerClass, uintptr_t playerIndex) {
+			spdlog::trace(__func__);
+			UnkSomePlayerUpdateFunc(unkPlayerClass, playerIndex);
+
+			intptr_t playerClass = unkPlayerClass;
+			
+		}//UnkSomePlayerUpdateFuncHook
 
 		void CreateHooks(size_t RealBaseAddr) {
 			//DEBUGNOW hitting some kind of exception on caps machine
 			missionCode = NULL;
 			try {
-				missionCode = (uint32_t*)((missionCode_Addr - BaseAddr) + RealBaseAddr);;
+				missionCode = (uint32_t*)((missionCode_Addr - BaseAddr) + RealBaseAddr);
 			}
 			catch (std::runtime_error & e) {
 				spdlog::error("CHP: runtime exception - {}", e.what());
@@ -24,6 +36,9 @@ namespace IHHook {
 				spdlog::error("CHP: missionCode==NULL");
 			}
 
+			//DEBUGNOW
+			//CREATEDETOURB(UnkSomeUpdateFunc)
+			//ENABLEHOOK(UnkSomeUpdateFunc)
 		}//CreateHooks
 	}//Hooks_TPP
 }//namespace IHHook
