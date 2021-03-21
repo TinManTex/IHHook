de475804d362a8abe7c361adcf317ab623d90041
TinManTex <tinman.squad@gmail.com>
2021-03-21 21:24:00 +1300

    added export GetdfDIJoystick, set private exports

--- IHHook/DInputProxy.cpp
+++ IHHook/DInputProxy.cpp
@@ -4,38 +4,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//DEBUGNOW put most of this into header
+//DEBUGNOW put this into a header or a DEF
 #pragma comment(linker, "/export:DirectInput8Create=DirectInput8Create")
-#pragma comment(linker, "/export:DllCanUnloadNow=DllCanUnloadNow")
-#pragma comment(linker, "/export:DllGetClassObject=DllGetClassObject")
-#pragma comment(linker, "/export:DllRegisterServer=DllRegisterServer")
-#pragma comment(linker, "/export:DllUnregisterServer=DllUnregisterServer")
+#pragma comment(linker, "/export:DllCanUnloadNow=DllCanUnloadNow,PRIVATE")
+#pragma comment(linker, "/export:DllGetClassObject=DllGetClassObject,PRIVATE")
+#pragma comment(linker, "/export:DllRegisterServer=DllRegisterServer,PRIVATE")
+#pragma comment(linker, "/export:DllUnregisterServer=DllUnregisterServer,PRIVATE")
+#pragma comment(linker, "/export:GetdfDIJoystick=GetdfDIJoystick")
 //DEBUGNOW not sure if these are correct mangled names. extern C exports unmangled anyway
 //#pragma comment(linker,"/EXPORT:DllCanUnloadNow=DllCanUnloadNow@0,PRIVATE")
 //#pragma comment(linker, "/EXPORT:DllGetClassObject=DllGetClassObject@12,PRIVATE")
 //#pragma comment(linker, "/EXPORT:DllRegisterServer=DllRegisterServer@0,PRIVATE")
 //#pragma comment(linker, "/EXPORT:DllUnregisterServer=DllUnregisterServer@0,PRIVATE")
 
-//GetdfDIJoystick//DEBUGNOW ????
-
 typedef HRESULT(WINAPI*DirectInput8Create_ptr)(HINSTANCE hinst, DWORD dwVersion, REFIID riidltf, LPVOID * ppvOut, void* punkOuter);
 //typedef HRESULT(WINAPI* DirectInput8Create_ptr)(HINSTANCE, DWORD, REFIID, LPVOID*, LPUNKNOWN);//DEBUGNOW CULL
-// typedef HRESULT(WINAPI* CreateDevice_ptr)(IDirectInput8*, REFGUID, LPDIRECTINPUTDEVICE, LPUNKNOWN);//DEBUGNOW CULL
+//typedef HRESULT(WINAPI* CreateDevice_ptr)(IDirectInput8*, REFGUID, LPDIRECTINPUTDEVICE, LPUNKNOWN);//DEBUGNOW CULL
 //typedef HRESULT(WINAPI* GetClassObject_ptr)(REFCLSID rclsid, REFIID riid, LPVOID* ppv);
 
 DirectInput8Create_ptr DirectInput8Create_orig = NULL;
-//GetClassObject_ptr GetClassObject_Orig = NULL;
 FARPROC DllCanUnloadNow_Orig;
 FARPROC DllGetClassObject_Orig;
 FARPROC DllRegisterServer_Orig;
 FARPROC DllUnregisterServer_Orig;
+FARPROC GetdfDIJoystick_Orig;
 
 
 
 extern HMODULE g_thisModule;
 bool origLoaded = false;
 HMODULE origDll = NULL;
-
 
 bool LoadProxiedDll()
 {
@@ -83,3 +81,4 @@ extern "C" __declspec(dllexport) void DllCanUnloadNow() { DllCanUnloadNow_Orig()
 extern "C" __declspec(dllexport) void DllGetClassObject() { DllGetClassObject_Orig(); }
 extern "C" __declspec(dllexport) void DllRegisterServer() { DllRegisterServer_Orig(); }
 extern "C" __declspec(dllexport) void DllUnregisterServer() { DllUnregisterServer_Orig(); }
+extern "C" __declspec(dllexport) void GetdfDIJoystick() { GetdfDIJoystick_Orig(); }
