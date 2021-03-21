f9f44b9ee3c7697eef0e809cc00e741b0080eae4
TinManTex <tinman.squad@gmail.com>
2021-03-21 16:30:55 +1300

    add missing dinput8 exports

--- IHHook/DInputProxy.cpp
+++ IHHook/DInputProxy.cpp
@@ -4,11 +4,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//DEBUGNOW put most of this into header
+#pragma comment(linker, "/export:DirectInput8Create=DirectInput8Create")
+#pragma comment(linker, "/export:DllCanUnloadNow=DllCanUnloadNow")
+#pragma comment(linker, "/export:DllGetClassObject=DllGetClassObject")
+#pragma comment(linker, "/export:DllRegisterServer=DllRegisterServer")
+#pragma comment(linker, "/export:DllUnregisterServer=DllUnregisterServer")
+//DEBUGNOW not sure if these are correct mangled names. extern C exports unmangled anyway
+//#pragma comment(linker,"/EXPORT:DllCanUnloadNow=DllCanUnloadNow@0,PRIVATE")
+//#pragma comment(linker, "/EXPORT:DllGetClassObject=DllGetClassObject@12,PRIVATE")
+//#pragma comment(linker, "/EXPORT:DllRegisterServer=DllRegisterServer@0,PRIVATE")
+//#pragma comment(linker, "/EXPORT:DllUnregisterServer=DllUnregisterServer@0,PRIVATE")
+
+//GetdfDIJoystick//DEBUGNOW ????
+
+typedef HRESULT(WINAPI*DirectInput8Create_ptr)(HINSTANCE hinst, DWORD dwVersion, REFIID riidltf, LPVOID * ppvOut, void* punkOuter);
+//typedef HRESULT(WINAPI* DirectInput8Create_ptr)(HINSTANCE, DWORD, REFIID, LPVOID*, LPUNKNOWN);//DEBUGNOW CULL
+// typedef HRESULT(WINAPI* CreateDevice_ptr)(IDirectInput8*, REFGUID, LPDIRECTINPUTDEVICE, LPUNKNOWN);//DEBUGNOW CULL
+//typedef HRESULT(WINAPI* GetClassObject_ptr)(REFCLSID rclsid, REFIID riid, LPVOID* ppv);
+
+DirectInput8Create_ptr DirectInput8Create_orig = NULL;
+//GetClassObject_ptr GetClassObject_Orig = NULL;
+FARPROC DllCanUnloadNow_Orig;
+FARPROC DllGetClassObject_Orig;
+FARPROC DllRegisterServer_Orig;
+FARPROC DllUnregisterServer_Orig;
+
+
+
 extern HMODULE g_thisModule;
-
-typedef HRESULT(*DirectInput8Create_ptr)(HINSTANCE hinst, DWORD dwVersion, REFIID riidltf, LPVOID * ppvOut, void* punkOuter);
-DirectInput8Create_ptr DirectInput8Create_orig;
-
 bool origLoaded = false;
 HMODULE origDll = NULL;
 
@@ -38,6 +62,10 @@ bool LoadProxiedDll()
 		return false;
 
 	DirectInput8Create_orig = (DirectInput8Create_ptr)GetProcAddress(origDll, "DirectInput8Create");
+	DllCanUnloadNow_Orig = GetProcAddress(origDll, "DllCanUnloadNow");
+	DllGetClassObject_Orig = GetProcAddress(origDll, "DllGetClassObject");
+	DllRegisterServer_Orig = GetProcAddress(origDll, "DllRegisterServer");
+	DllUnregisterServer_Orig = GetProcAddress(origDll, "DllUnregisterServer");
 
 	origLoaded = true;
 	return true;
@@ -50,3 +78,8 @@ extern "C" __declspec(dllexport) HRESULT DirectInput8Create(HINSTANCE hinst, DWO
 
 	return DirectInput8Create_orig(hinst, dwVersion, riidltf, ppvOut, punkOuter);
 }
+
+extern "C" __declspec(dllexport) void DllCanUnloadNow() { DllCanUnloadNow_Orig(); }
+extern "C" __declspec(dllexport) void DllGetClassObject() { DllGetClassObject_Orig(); }
+extern "C" __declspec(dllexport) void DllRegisterServer() { DllRegisterServer_Orig(); }
+extern "C" __declspec(dllexport) void DllUnregisterServer() { DllUnregisterServer_Orig(); }
