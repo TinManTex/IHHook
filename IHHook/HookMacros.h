7a241f63872773494b0f400b625dd1c11cd58e20
TinManTex <tinman.squad@gmail.com>
2021-03-31 16:33:19 +1300

    hookmacros rework
    split function pointer decl + base address assign to decl and seperate func address var
    Which means no futzing around with DETOUR vs DETOURB

--- IHHook/HookMacros.h
+++ IHHook/HookMacros.h
@@ -6,16 +6,18 @@
 //and an extern function pointer of the type
 //so you can include a header with the hookfunc in other files and use the function
 #define FUNCPTRDEF(ret, name, ...) typedef ret ( __fastcall name##Func ) (__VA_ARGS__);\
-extern name##Func* name;
+extern name##Func* name;\
+extern name##Func* name##Addr;\
 //Example use:
 //FUNCPTRDEF(lua_newstate, 0x14cdd7ab0, LuaState*, lua_Alloc f, void *ud);
 //Expands to:
 //typedef lua_State* (__fastcall *lua_newstateFunc)(lua_Alloc f, void *ud);
-//lua_newstateFunc lua_newstate;
+//extern lua_newstateFunc lua_newstate;
 
 //tex declare function pointer, and set to address in respect to module base, this will be rebased and overwritten at runtime using CREATEHOOK. Kinda hinky, but it saves having to set up yet another variable.
 //since it's actually setting value this macro cant be in header/must be in a cpp else linker will complain about the muli decls
-#define HOOKPTR(name, address) name##Func* name = (name##Func*)address;
+#define HOOKPTR(name, address) name##Func* name = (name##Func*)address;\
+name##Func* name##Addr = (name##Func*)address;\
 //Example use:
 //HOOKPTR(lua_newstate, 0x14cdd7ab0);
 //Expands to:
@@ -25,29 +27,25 @@ extern name##Func* name;
 //tex following macros used in create hooks functions at runtime, requires setup with HOOK* macros
 
 //tex dont need detour, just want original function
-#define CREATEHOOK(name) void* name##Rebased = (void*)(((size_t)name - BaseAddr) + RealBaseAddr); \
-name = (name##Func*)name##Rebased;
+#define CREATEHOOK(name) void* name##AddrRebased = (void*)(((size_t)name##Addr - BaseAddr) + RealBaseAddr); \
+name = (name##Func*)name##AddrRebased;
 //Example use:
 //CREATEHOOK(lua_newstate);
 //Expands to:
-//void* lua_newstateRebased = (void*)((lua_newstateAddr - BaseAddr) + RealBaseAddr);
-//lua_newstate = (lua_newstateFunc)lua_newstateRebased;
+//void* lua_newstateAddrRebased = (void*)((lua_newstateAddr - BaseAddr) + RealBaseAddr);
+//lua_newstate = (lua_newstateFunc)lua_newstateAddrRebased;
 
 
 //tex detour and trampoline via MinHook
 // Function addresses are from IDA/Ghidra, which uses the ImageBase field in the exe as the base address (usually 0x140000000)
 // the real base address changes every time the game is run though, so we have to remove that base address and add the real one
-#define CREATEDETOUR(name) void* name##Rebased = (void*)(((size_t)name - BaseAddr) + RealBaseAddr);\
+#define CREATEDETOURB(name) void* name##Rebased = (void*)(((size_t)name##Addr - BaseAddr) + RealBaseAddr);\
 MH_CreateHook(name##Rebased, name##Hook, reinterpret_cast<LPVOID*>(&name));
 //Example use:
 //CREATEHOOK(lua_newstate);
 //Expands to:
 //void* lua_newstateAddrRebased = (void*)((lua_newstateAddr - BaseAddr) + RealBaseAddr);
-//MH_CreateHook(lua_newstateAddrRebased, lua_newstateHook, (LPVOID*)&lua_newstate);
-
-//tex ASSUMES CREATEHOOK has already rebased address, still defines name##Rebased since MH_EnableHook wants the original address and MH_CreateHook will be overwriting the pointer since I'm reusing original name.
-#define CREATEDETOURB(name) void* name##Rebased = (void*)name;\
-MH_CreateHook((void*)name, name##Hook, reinterpret_cast<LPVOID*>(&name));
+//MH_CreateHook(lua_newstateRebased, lua_newstateHook, (LPVOID*)&lua_newstate);
 
 //tex ASSUMES CREATEDETOUR has defined name##Rebased
 #define ENABLEHOOK(name) MH_EnableHook(name##Rebased);
