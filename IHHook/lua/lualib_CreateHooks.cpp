d5477b7911224e7c1e6df22e6aaa3e6c2ae8ca40
TinManTex <tinman.squad@gmail.com>
2021-04-06 19:59:49 +1200

    lua isTargetExe addr or dig addr

--- IHHook/lua/lualib_CreateHooks.cpp
+++ IHHook/lua/lualib_CreateHooks.cpp
@@ -2,25 +2,82 @@
 #include <lualib.h>
 
 namespace IHHook {
-void CreateHooks_Lualib(size_t BaseAddr, size_t RealBaseAddr) {
-	//lualib.h
+	namespace lualib {
+		void GetRebasedAddresses(size_t BaseAddr, size_t RealBaseAddr) {
+			//lualib.h
 
-	CREATE_FUNCPTR(luaopen_base)//TEST
+			GET_REBASED_ADDR(luaopen_base)//TEST
 
-	CREATE_FUNCPTR(luaopen_table)//TEST
+			GET_REBASED_ADDR(luaopen_table)//TEST
 
-	CREATE_FUNCPTR(luaopen_io)//TEST
+			GET_REBASED_ADDR(luaopen_io)//TEST
 
-	CREATE_FUNCPTR(luaopen_os)//TEST
+			GET_REBASED_ADDR(luaopen_os)//TEST
 
-	CREATE_FUNCPTR(luaopen_string)//TEST
+			GET_REBASED_ADDR(luaopen_string)//TEST
 
-	CREATE_FUNCPTR(luaopen_math)//TEST
+			GET_REBASED_ADDR(luaopen_math)//TEST
 
-	CREATE_FUNCPTR(luaopen_debug)//TEST
+			GET_REBASED_ADDR(luaopen_debug)//TEST
 
-	CREATE_FUNCPTR(luaopen_package)//TEST
+			GET_REBASED_ADDR(luaopen_package)//TEST
 
-	CREATE_FUNCPTR(luaL_openlibs)
-}//CreateHooks_Luaxlib
+			GET_REBASED_ADDR(luaL_openlibs)
+		}//GetRebasedAddresses
+
+		void GetSigAddresses() {
+			//lualib.h
+
+			GET_SIG_ADDR(luaopen_base)//TEST
+
+			GET_SIG_ADDR(luaopen_table)//TEST
+
+			GET_SIG_ADDR(luaopen_io)//TEST
+
+			GET_SIG_ADDR(luaopen_os)//TEST
+
+			GET_SIG_ADDR(luaopen_string)//TEST
+
+			GET_SIG_ADDR(luaopen_math)//TEST
+
+			GET_SIG_ADDR(luaopen_debug)//TEST
+
+			GET_SIG_ADDR(luaopen_package)//TEST
+
+			GET_SIG_ADDR(luaL_openlibs)
+		}//GetSigAddresses
+		void CreateFuncPtrs() {
+			//lualib.h
+
+			CREATE_FUNCPTR(luaopen_base)//TEST
+
+			CREATE_FUNCPTR(luaopen_table)//TEST
+
+			CREATE_FUNCPTR(luaopen_io)//TEST
+
+			CREATE_FUNCPTR(luaopen_os)//TEST
+
+			CREATE_FUNCPTR(luaopen_string)//TEST
+
+			CREATE_FUNCPTR(luaopen_math)//TEST
+
+			CREATE_FUNCPTR(luaopen_debug)//TEST
+
+			CREATE_FUNCPTR(luaopen_package)//TEST
+
+			CREATE_FUNCPTR(luaL_openlibs)
+		}//CreateFuncPtrs
+	}//namespace lualib
+
+	extern bool isTargetExe;
+	void CreateHooks_Lualib(size_t BaseAddr, size_t RealBaseAddr) {
+		if (isTargetExe) {
+			lualib::GetRebasedAddresses(BaseAddr, RealBaseAddr);
+		}
+		else {
+			lualib::GetSigAddresses();
+		}
+
+		lualib::CreateFuncPtrs();
+	}//CreateHooks_Lualib
 }//namespace IHHook
