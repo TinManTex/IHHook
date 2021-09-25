5179b751dfa403a43542fbf635f2fe1879c7e7c4
TinManTex <tinman.squad@gmail.com>
2021-09-25 12:00:14 +1200

    Create mgsvtpp_func_typedefs_manual.h

---
+++ IHHook/hooks/mgsvtpp_func_typedefs_manual.h
@@ -0,0 +1,14 @@
+#pragma once
+//Not generated, manually managed version of the generated version
+//for wrangling with stuff you skipped via exportFunc:False
+//Typdefs and externs for the function pointers as well as detour function declaration (not func ptrs)
+
+#include "lua/lua.h"//l_StubbedOutFunc TODO: dont like this
+
+typedef int (__fastcall l_StubbedOutFunc)(lua_State * L);
+typedef void (__fastcall nullsub_2Func)(const char * unkSomeIdStr, longlong unkSomeIdNum);
+
+//CULL, handled in generated version
+//tex the (extern of the) function pointers
+//extern l_StubbedOutFunc* l_StubbedOut;//EXPORT_FUNC_FALSE
+//extern nullsub_2Func* nullsub_2;//EXPORT_FUNC_FALSE
