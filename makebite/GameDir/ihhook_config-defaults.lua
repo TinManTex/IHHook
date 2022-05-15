be3fe09f0a25d77de79f6be554f5516d6e11721f
TinManTex <tinman.squad@gmail.com>
2022-05-15 12:42:07 +1200

    explanatory comments

--- makebite/GameDir/ihhook_config-defaults.lua
+++ makebite/GameDir/ihhook_config-defaults.lua
@@ -1,4 +1,11 @@
---ihhook_config.lua
+--ihhook_config-defaults.lua
+--ihhook_config lets you set some start up values for ihhoook
+--Currently for debug/development options, nothing the average user needs to bother with.
+--This file the defaults/example for ihhook_config
+--Rename to ihhook_config.lua to use
+--Should be in same folder as ihhook/dinput8 dll
+--Even though this is .lua, IHHook has a bespoke and fragile parsing method for this file rather than an actual lua loader
+--So stick close to the provided format - dont move the brackets, dont put any spaces before/after the = 
 local this={
 	debugMode=true,
 	openConsole=false,
